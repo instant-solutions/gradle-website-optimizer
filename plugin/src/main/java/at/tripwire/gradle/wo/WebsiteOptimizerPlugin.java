c04a67a7a36a42f54eeffc06446bf5ac378caab7
David Andlinger <david.andlinger@instant-it.at>
2015-04-15 20:35:00 +0200

    working on gradle plugin

---
+++ plugin/src/main/java/at/tripwire/gradle/wo/WebsiteOptimizerPlugin.java
@@ -0,0 +1,12 @@
+package at.tripwire.gradle.wo;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class WebsiteOptimizerPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+    }
+}
